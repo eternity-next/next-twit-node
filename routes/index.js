e95d98054943df732f0c9a5a163c9af5af4fa77d
eternity-next <snatcher@nhn.com>
2014-07-17 18:27:01 +0900

    login, signup 페이지 작성

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/home', function(req, res) {
   res.render('home');
 });
 
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup');
+});
+
 module.exports = router;
